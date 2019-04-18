f5d41252b9547cbe92b3e6366bf5481ce9a4271d
8c6t <nkw8620@gmail.com>
2019-04-18 11:36:15 +0900

    #3.1 첫 컴포넌트 생성
    MyComponent 생성, export, import

---
+++ ch03/hello-component/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        내 컴포넌트
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
