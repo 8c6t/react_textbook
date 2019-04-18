78e370b492d1b57bb3f1b3556fc222733b8020bc
8c6t <nkw8620@gmail.com>
2019-04-18 14:20:16 +0900

    #3.3 state

--- ch03/hello-component/src/MyComponent.js
+++ ch03/hello-component/src/MyComponent.js
@@ -12,11 +12,32 @@ class MyComponent extends Component {
     age:  PropTypes.number.isRequired  // 필수적으로 존재해야 함
   }
 
+/*   
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0
+    }
+  } 
+*/
+
+  // transform-class-properties
+  state = {
+    number: 0
+  }
+
   render() {
     return (
       <div>
         <p>안녕하세요. 제 이름은 {this.props.name} 입니다.</p>
         <p>저는 {this.props.age}살입니다. </p>
+        <p>숫자: {this.state.number}</p>
+        <button onClick={() => {
+            this.setState({
+              number: this.state.number + 1
+            })
+          }
+        }>더하기</button>
       </div>
     );
   }
