1a616187312d017bfe1b597c28bc825db128071b
8c6t <nkw8620@gmail.com>
2019-04-18 10:53:47 +0900

    #2.4.3 if 문 대신 조건부 연산자
    jsx 내 조건부(삼항) 연산자 사용.

--- ch02/hello-react/src/App.js
+++ ch02/hello-react/src/App.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     const text = '자바스크립트 표현식 사용';
+    const condition = true;
     return (
       <div>
         <h1>리액트 안녕</h1>
         <h2>{text}</h2>
+        {
+          condition ? '참' : '거짓'
+        }
       </div>
     );
   }
