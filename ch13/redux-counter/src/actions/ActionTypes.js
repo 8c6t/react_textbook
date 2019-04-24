b163fdef37a35a0a6c2d0b2953c10b5efe0fff84
8c_6t <nkw8620@gmail.com>
2019-04-24 15:24:52 +0900

    #13.1.5 액션 생성

---
+++ ch13/redux-counter/src/actions/ActionTypes.js
@@ -0,0 +1,9 @@
+/* 
+  Action 종류들을 선언
+  앞에 export를 붙이면 나중에 불러올 때,
+  import * as types from './ActionTypes' 로 불러올 수 있음
+*/
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
