fc693c75d18cf43438dc530e699fba0362c7d5c8
8c_6t <nkw8620@gmail.com>
2019-04-26 13:24:37 +0900

    #13.1.10 서브 리듀서 생성

--- ch13/redux-counter/src/reducers/index.js
+++ ch13/redux-counter/src/reducers/index.js
@@ -1,28 +1,15 @@
-import * as types from '../actions/ActionTypes';
-
-// 초기 상태 정의
-const initialState = {
-  color: 'black',
-  number: 0
-}
+import number from './number';
+import color from './color';
+import { combineReducers } from 'redux';
 
 /* 
-  리듀서 함수 정의
-  state가 undefined일때(스토어가 생성될 때) state 기본값을 initialState로 사용
-  action.type에 따라 다른 작업을 하고, 새 상태를 만들어서 반환
-  이때, state를 직접 수정하지 않고, 기존 상태 값에 원하는 값을 덮어쓴 새로운 객체를 만들어서 반환
+  서브 리듀서들을 하나로 합침
+  combineReducers를 실행하고 나면,
+  나중에 store 형태를 파라미터로 전달한 객체 모양대로 만듬
 */
-function counter(state = initialState, action) {
-  switch(action.type) {
-    case types.INCREMENT:
-      return { ...state, number: state.number + 1 };
-    case types.DECREMENT:
-      return { ...state, number: state.number - 1 };
-    case types.SET_COLOR:
-      return { ...state, color: action.color };
-    default:
-      return state;
-  }
-};
+const reducers = combineReducers({
+  numberData: number,
+  colorData: color
+});
 
-export default counter;
+export default reducers;
