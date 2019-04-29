bf60e4abc78a31d591ad3c08602f1ce660c40ece
8c_6t <nkw8620@gmail.com>
2019-04-29 18:00:53 +0900

    #15.2.2.5 요청 완료 후, 오류가 발생했을 때 추가할 작업

--- ch15/redux-starter-kit/src/App.js
+++ ch15/redux-starter-kit/src/App.js
@@ -2,23 +2,52 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-import axios from 'axios';
+import * as postActions from './modules/post';
 
 class App extends Component {
+
+    loadData = async () => {
+        const { PostActions, number } = this.props;
+        try {
+            const response = await PostActions.getPost(number);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-            .then(response => console.log(response));
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // 이전 number와 현재 number가 다르면 요청을 시작
+        if(this.props.number !== prevProps.number) {
+            this.loadData();
+        }
     }
 
     render() {
-        const { CounterActions, number } = this.props;
-
+        const { CounterActions, number, post, error, loading } = this.props;
         
         return (
             <div>
                 <h1>{number}</h1>
-                <button onClick={CounterActions.incrementAsync}>+</button>
-                <button onClick={CounterActions.decrementAsync}>-</button>
+                {/* 즉시 실행 함수 */}
+                { 
+                    (() => {
+                        if(loading) return (<h2>로딩중...</h2>);
+                        if(error)   return (<h2>오류 발생!</h2>);
+                        return (
+                            <div>
+                                <h2>{post.title}</h2>
+                                <p>{post.body}</p>
+                            </div>
+                        )
+                    })()
+                }
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
             </div>
         );
     }
@@ -26,9 +55,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
