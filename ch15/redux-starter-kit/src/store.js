1cdb52fd79e51f803fd622a617efa6d1411e8796
8c_6t <nkw8620@gmail.com>
2019-04-29 16:38:26 +0900

    #15.1.2.4 redux-logger 라이브러리 사용

--- ch15/redux-starter-kit/src/store.js
+++ ch15/redux-starter-kit/src/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleaware';
 
-/* 
-  미들웨어가 여럿일 경우 파라미터로 전달. ex: applyMiddleware(a,b,c)
-  미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정됨
+import { createLogger } from 'redux-logger';
+
+/*
+  로그 미들웨어를 만들 때 설정을 커스터마이징 할 수 있음
+  https://github.com/evgenyrodionov/redux-logger#options
 */
-const store = createStore(modules, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger));
 
 export default store;
