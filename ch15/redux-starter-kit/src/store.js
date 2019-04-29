0640328d08e1972d2aeaf759e28d6ef4f8c66f5c
8c_6t <nkw8620@gmail.com>
2019-04-29 16:32:32 +0900

    #15.1.2.3 미들웨어 적용

--- ch15/redux-starter-kit/src/store.js
+++ ch15/redux-starter-kit/src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
+import loggerMiddleware from './lib/loggerMiddleaware';
 
-const store = createStore(modules)
+/* 
+  미들웨어가 여럿일 경우 파라미터로 전달. ex: applyMiddleware(a,b,c)
+  미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정됨
+*/
+const store = createStore(modules, applyMiddleware(loggerMiddleware));
 
 export default store;
